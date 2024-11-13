ea725af9335f08d944a79cffa97f32ec9db0a9b2
= <=>
2024-11-13 19:43:53 +0300

    feat: add background color change on button click

---
+++ script.js
@@ -0,0 +1,28 @@
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+const main = document.querySelector("main");
+let value = "#FFFFFF";
+
+// btn.addEventListener("click", function () {
+//   main.style["background-color"] = color.innerHTML.trim().toUpperCase();
+// });
+
+color.addEventListener("focus", function () {
+  value = color.innerHTML;
+});
+
+color.addEventListener("blur", function () {
+  const potentialValue = color.innerHTML;
+
+  if (/^#[0-9A-F]{6}$/i.test(formatHexColor(potentialValue))) {
+    value = potentialValue;
+  } else {
+    color.innerHTML = value;
+  }
+
+  main.style["background-color"] = formatHexColor(value);
+});
+
+function formatHexColor(text) {
+  return text.trim().toUpperCase();
+}
